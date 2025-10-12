129f1852b18adcd5b2fe2a3461cbf02cbf976565
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-12 07:52:54 +0530

    DataTypes int, float, byte, char etc.

--- Lec06_Datatypes/_04_Char_Demo.java
+++ Lec06_Datatypes/_04_Char_Demo.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class _04_Char_Demo {
     public static void main(String[] args) {
         char ch = 'a';
+		System.out.println(ch);
 		char ch1 = '?';
-		Scanner sc = new Scanner(System.in);
-		ch = sc.next().charAt(0);// input
-
-		System.out.println(ch);
-		System.out.println(ch1);
-		System.out.println((int) (ch));
-		System.out.println((int) (ch1));
-		System.out.println(ch + ch1);
-		int x = 19;
-		x++;// (int)(x+1)//implicit
-		++ch;// ch = (char)(ch+1)//implicit
-		System.out.println(ch);
-		ch = (char) (ch + 1);// Explicit
-		ch += 1;// ch= (char)(ch+1); //implicit
-		System.out.println(ch);
-		char g = (char)(ch+ch1);
-		System.out.println(g);
-
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            ch = sc.next().charAt(0);// input
+            
+            System.out.println(ch);
+            System.out.println(ch1);
+            System.out.println((int) (ch));
+            System.out.println((int) (ch1));
+            System.out.println(ch + ch1);
+            int x = 19;
+            x++;// (int)(x+1)//implicit
+            ++ch;// ch = (char)(ch+1)//implicit
+            System.out.println(ch);
+            ch = (char) (ch + 1);// Explicit
+            ch += 1;// ch= (char)(ch+1); //implicit
+            System.out.println(ch);
+            char g = (char)(ch+ch1);
+            System.out.println(g);
+            System.out.println(x);
+        } // input
     }
 }

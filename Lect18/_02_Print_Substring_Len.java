1ba9206193e956f5fe38bf90790467a03ef6ea56
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-14 08:31:07 +0530

    String QS

---
+++ Lect18/_02_Print_Substring_Len.java
@@ -0,0 +1,20 @@
+// // package Lect18;
+
+// public class _02_Print_Substring_Len {
+//     public static void main(String[] args) {
+//         String str = "84513";
+//         PrintSubString(str);
+
+//     }
+
+//     public static void PrintSubString(String s) {
+//         for (int len = 1; len <= s.length(); len++) {
+//             for (int j = len; j <= s.length(); j++) {
+//                 int i = j - len;
+//                 System.out.println(s.substring(i, j) + " ");
+//             }
+
+//         }
+
+//     }
+// }

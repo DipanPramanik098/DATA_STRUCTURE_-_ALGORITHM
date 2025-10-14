369e7a2f27f11dfe9e1511911d71eb3416d0deb9
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-14 07:41:49 +0530

    comment

--- Lec13_Binary_Search/_01_Binary_Search.java
+++ Lec13_Binary_Search/_01_Binary_Search.java
@@ -1,28 +1,28 @@
-package Lec13_Binary_Search;
+// // package Lec13_Binary_Search;
 
-public class _01_Binary_Search {
-    public static void main(String[] args) {
-        int[] arr = { 2, 3, 5, 6, 7, 8, 9, 11, 13, 15, 18 };
-        int item = 11;
-        System.out.println(Search(arr, item));
+// public class _01_Binary_Search {
+//     public static void main(String[] args) {
+//         int[] arr = { 2, 3, 5, 6, 7, 8, 9, 11, 13, 15, 18 };
+//         int item = 11;
+//         System.out.println(Search(arr, item));
 
-    }
+//     }
 
-    public static int Search(int[] arr, int item) {
-        int lo = 0;
-        int hi = arr.length - 1;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (arr[mid] == item) {
-                return mid;
-            } else if (arr[mid] > item) {
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
+//     public static int Search(int[] arr, int item) {
+//         int lo = 0;
+//         int hi = arr.length - 1;
+//         while (lo <= hi) {
+//             int mid = (lo + hi) / 2;
+//             if (arr[mid] == item) {
+//                 return mid;
+//             } else if (arr[mid] > item) {
+//                 hi = mid - 1;
+//             } else {
+//                 lo = mid + 1;
+//             }
 
-        }
-        return -1;
+//         }
+//         return -1;
 
-    }    
-}
+//     }    
+// }

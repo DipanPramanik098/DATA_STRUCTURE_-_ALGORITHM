55f98a797e49b2f9951c3269065f2e8af93d7ecd
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-14 07:44:59 +0530

    2D array Demo

---
+++ Lec14_2D_Array/_01_TwoD_arr.java
@@ -0,0 +1,27 @@
+// // package Lec14_2D_Array;
+
+// public class _01_TwoD_arr {
+//     public static void main(String[] args) {
+//         int[][] arr = new int[3][4];
+//         System.out.println(arr);
+//         System.out.println(arr[1]);
+//         System.out.println(arr[0]);
+//         System.out.println(arr[1][2]);
+//         int[][] other = arr;
+//         Display(arr);
+
+//     }
+
+//     public static void Display(int[][] akanksha) {
+//         // int r=akanksha.length;// row
+//         // int c = akanksha[0].length;//col
+//         for (int i = 0; i < akanksha.length; i++) {
+//             for (int j = 0; j < akanksha[0].length; j++) {
+//                 System.out.print(akanksha[i][j] + " ");
+
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
---
+++ Lec14_2D_Array/_02_UserInput.java
@@ -0,0 +1,32 @@
+// // package Lec14_2D_Array;
+
+// import java.util.Scanner;
+
+// public class _02_UserInput {
+//     public static void main(String[] args) {
+// 		Scanner sc = new Scanner(System.in);
+// 		int r = sc.nextInt();
+// 		int c = sc.nextInt();
+// 		int[][] arr = new int[r][c];
+// 		for (int i = 0; i < arr.length; i++) {
+// 			for (int j = 0; j < arr[0].length; j++) {
+// 				arr[i][j] = sc.nextInt();
+// 			}
+// 		}
+// 		Display(arr);
+
+// 	}
+
+// 	public static void Display(int[][] akanksha) {
+// //		int r=akanksha.length;// row
+// //		int c = akanksha[0].length;//col
+// 		for (int i = 0; i < akanksha.length; i++) {
+// 			for (int j = 0; j < akanksha[0].length; j++) {
+// 				System.out.print(akanksha[i][j] + " ");
+
+// 			}
+// 			System.out.println();
+// 		}
+
+// 	}
+// }

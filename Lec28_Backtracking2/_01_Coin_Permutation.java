a45366670c05e5568d49b05db11f52fe34379459
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-17 14:24:00 +0530

    Coin Problems

---
+++ Lec28_Backtracking2/_01_Coin_Permutation.java
@@ -0,0 +1,24 @@
+// // package Lec28_Backtracking2;
+
+// public class _01_Coin_Permutation {
+//     public static void main(String[] args) {
+//         int[] coin = { 2, 1, 3, 5 };
+//         int amount = 4;
+//         Permutation(coin, amount, "");
+//     }
+//     public static void Permutation(int [] coin, int amount, String ans){
+//         if(amount == 0){
+//             System.out.println(ans);
+//             return;
+//         }
+//         // if(amount < 0){
+//         //     return;
+//         // }
+
+//         for(int i=0; i<coin.length; i++){
+//             if(amount>=coin[i]){
+//                 Permutation(coin, amount - coin[i], ans + coin[i]);
+//             }
+//         }
+//     }
+// }

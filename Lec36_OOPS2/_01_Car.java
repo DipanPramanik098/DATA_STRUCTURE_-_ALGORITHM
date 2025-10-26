63bcb7c36d28e24ae149c3e9fd6500c43d4fcc59
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-26 12:33:24 +0530

    access modifiers, exception, try catch, finally, Stack intro

--- Lec36_OOPS2/_01_Car.java
+++ Lec36_OOPS2/_01_Car.java
@@ -1,79 +1,79 @@
 
-public class _01_Car {
+// public class _01_Car {
 
-    private String color;
-    private int price;
-    private int speed;
+//     private String color;
+//     private int price;
+//     private int speed;
 
-    // note: Constructor
-    public _01_Car(String color, int price, int speed) {
-        this.color = color;
-        this.price = price;
-        this.speed = speed;
-    }
+//     // note: Constructor
+//     public _01_Car(String color, int price, int speed) {
+//         this.color = color;
+//         this.price = price;
+//         this.speed = speed;
+//     }
 
-    public void Displaycar() {
-        System.out.println("c " + this.color + " P " + this.price + " s " + this.speed);
-    }
+//     public void Displaycar() {
+//         System.out.println("c " + this.color + " P " + this.price + " s " + this.speed);
+//     }
 
-    public String getColor() {
-        return color;
-    }
+//     public String getColor() {
+//         return color;
+//     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+//     public void setColor(String color) {
+//         this.color = color;
+//     }
 
-    public int getPrice() {
-        return price;
-    }
-    // public void setPrice(int price) throws Exception{
-    // if (price < 0) {
-    //
-    // throw new Exception("Bklol price kabhi -ve nhi hota hai");
-    // }
-    // this.price = price;
-    // }
+//     public int getPrice() {
+//         return price;
+//     }
+//     // public void setPrice(int price) throws Exception{
+//     // if (price < 0) {
+//     //
+//     // throw new Exception("Bklol price kabhi -ve nhi hota hai");
+//     // }
+//     // this.price = price;
+//     // }
     
-    public void setPrice(int price) {
+//     public void setPrice(int price) {
 
-        try {
-            if (price < 0) {
+//         try {
+//             if (price < 0) {
 
-                throw new Exception("Bklol price kabhi -ve nhi hota hai");
-            }
-            this.price = price;
-        }
+//                 throw new Exception("Bklol price kabhi -ve nhi hota hai");
+//             }
+//             this.price = price;
+//         }
 
-        catch (Exception e) {
-            System.out.println("In catch ");
-            // e.printStackTrace();
+//         catch (Exception e) {
+//             System.out.println("In catch ");
+//             // e.printStackTrace();
 
-        }
+//         }
 
-        finally {
-            System.out.println("i am in finaly");
-        }
+//         finally {
+//             System.out.println("i am in finaly");
+//         }
 
-    }
+//     }
 
-    public int getSpeed() {
+//     public int getSpeed() {
 
-        return speed;
-    }
+//         return speed;
+//     }
 
-    public void setSpeed(int speed) {
-        if (speed < 0) {
-            System.out.println("Bklol Speed -ve nhi hoga");
-            return;
-        }
-        this.speed = speed;
-    }
-}
+//     public void setSpeed(int speed) {
+//         if (speed < 0) {
+//             System.out.println("Bklol Speed -ve nhi hoga");
+//             return;
+//         }
+//         this.speed = speed;
+//     }
+// }
 
-// Note: Access Modifier
-// info: Modifier	SameClass SamePackage	Subclass OutsidePackage    
-//        public      ✅         ✅           ✅     ✅
-//        protected   ✅         ✅           ✅     ❌
-//        default     ✅         ✅           ❌     ❌
-//        private     ✅         ❌           ❌     ❌
+// // Note: Access Modifier
+// // info: Modifier	SameClass SamePackage	Subclass OutsidePackage    
+// //        public      ✅         ✅           ✅     ✅
+// //        protected   ✅         ✅           ✅     ❌
+// //        default     ✅         ✅           ❌     ❌
+// //        private     ✅         ❌           ❌     ❌

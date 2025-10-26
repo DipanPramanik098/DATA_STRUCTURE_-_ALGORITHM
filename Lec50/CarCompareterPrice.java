107c80495adc0f714ca366ee392fb1e4a39207fb
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-26 15:15:54 +0530

    OOps end - Coparator

---
+++ Lec50/CarCompareterPrice.java
@@ -0,0 +1,14 @@
+// // package Lec50;
+
+// import java.util.Comparator;
+
+// // import Lec50.Cars;
+
+// public class CarCompareterPrice implements Comparator<Cars> {
+
+// 	@Override
+// 	public int compare(Cars o1, Cars o2) {
+// 		return o2.price - o1.price;
+// 	}
+
+// }
---
+++ Lec50/CarCompareterSpeed.java
@@ -0,0 +1,14 @@
+// // package Lec50;
+
+// import java.util.Comparator;
+
+// // import Lec50.Cars;
+
+// public class CarCompareterSpeed implements Comparator<Cars> {
+
+// 	@Override
+// 	public int compare(Cars o1, Cars o2) {
+// 		return o1.speed - o2.speed;
+// 	}
+
+// }

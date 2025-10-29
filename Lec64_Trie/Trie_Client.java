dc457c95176c30572ae12d6aef87119871485aea
DipanPramanik098 <myselfdipan6@gmail.com>
2025-10-30 03:12:59 +0530

    Trie & QS

---
+++ Lec64_Trie/Trie_Client.java
@@ -0,0 +1,16 @@
+// // package Lec64_Trie;
+
+// public class Trie_Client {
+//     public static void main(String[] args) {
+//         Trie t = new Trie();
+//         t.insert("apple");
+//         t.insert("shivani");
+//         t.insert("raju");
+//         t.insert("raja");
+//         t.insert("mango");
+//         t.insert("go");
+//         System.out.println(t.search("app"));
+//         System.out.println(t.startsWith("app"));
+
+//     }
+// }

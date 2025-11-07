5bd536c24893c707cc99d9d430ed3fa570194acd
DipanPramanik098 <myselfdipan6@gmail.com>
2025-11-07 05:31:11 +0530

    Stack

--- Lec37_Stack&Queue_Implementation/_01_MyStack.java
+++ Lec37_Stack&Queue_Implementation/_01_MyStack.java
@@ -1,57 +1,57 @@
-public class _01_MyStack {
-    protected  int arr[];
-    protected int top = -1;
+// public class _01_MyStack {
+//     protected  int arr[];
+//     protected int top = -1;
 
-    public _01_MyStack() {
-		arr = new int[5];
-	}
+//     public _01_MyStack() {
+// 		arr = new int[5];
+// 	}
 
-    public _01_MyStack(int n) {
-		arr = new int[n];
-	}
+//     public _01_MyStack(int n) {
+// 		arr = new int[n];
+// 	}
 
-    public boolean isFull(){
-        return top == arr.length-1;
-    }
-    public boolean isEmpty(){
-        return top == -1;
-    }
+//     public boolean isFull(){
+//         return top == arr.length-1;
+//     }
+//     public boolean isEmpty(){
+//         return top == -1;
+//     }
 
-    public void push(int item) throws Exception{
-        if(isFull()){
-            throw new Exception("Stack is full");
-        }
-        top++;
-        arr[top] = item;
-        System.out.println(item +" added in the stack");
-    }
+//     public void push(int item) throws Exception{
+//         if(isFull()){
+//             throw new Exception("Stack is full");
+//         }
+//         top++;
+//         arr[top] = item;
+//         System.out.println(item +" added in the stack");
+//     }
 
-    public int pop() throws Exception{
-        if(isEmpty()){
-            throw new Exception("Stack is Empty");
-        }
-        int item = arr[top];
-        top--;
-        return item;
-    }
+//     public int pop() throws Exception{
+//         if(isEmpty()){
+//             throw new Exception("Stack is Empty");
+//         }
+//         int item = arr[top];
+//         top--;
+//         return item;
+//     }
 
-    public int peek() throws Exception {
-        if (isEmpty()) {
-            throw new Exception("Bklol Stack Empty hai ");
-        }
-        int rv = arr[top];
+//     public int peek() throws Exception {
+//         if (isEmpty()) {
+//             throw new Exception("Bklol Stack Empty hai ");
+//         }
+//         int rv = arr[top];
 
-        return rv;
-    }
+//         return rv;
+//     }
 
-    public int size() {
-        return top + 1;
-    }
+//     public int size() {
+//         return top + 1;
+//     }
 
-    public void Dispaly() {
-        for (int i = 0; i <= top; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-}
+//     public void Dispaly() {
+//         for (int i = 0; i <= top; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+// }

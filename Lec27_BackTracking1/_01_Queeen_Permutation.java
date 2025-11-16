82b9a39536a916fe5d75293d0f924e48ad46a0e8
DipanPramanik098 <myselfdipan6@gmail.com>
2025-11-16 13:52:06 +0530

    Backtracking more

--- Lec27_BackTracking1/_01_Queeen_Permutation.java
+++ Lec27_BackTracking1/_01_Queeen_Permutation.java
@@ -1,24 +1,24 @@
-// // package Lec27_BackTracking1;
+// package Lec27_BackTracking1;
 
-// public class _01_Queeen_Permutation {
-//     public static void main(String[] args) {
-//         int n = 4;
-//         int tq = 2;// total queen
-//         boolean[] board = new boolean[n];
-//         permutation(board, tq, 0, "");
-//     }    
+public class _01_Queeen_Permutation {
+    public static void main(String[] args) {
+        int n = 4;
+        int tq = 2;// total queen
+        boolean[] board = new boolean[n];
+        permutation(board, tq, 0, "");
+    }    
     
-//     public static void permutation(boolean[] board, int tq, int qpsf, String ans) {
-//         if (qpsf == tq){
-//             System.out.println(ans);
-//             return;
-//         }
-//         for(int i=0;i<board.length;i++){
-//             if(board[i] == false){
-//                 board[i] = true; 
-//                 permutation(board, tq, qpsf+1, ans + " q"+qpsf + "_b"+ i);
-//                 board[i] = false; //backtrack
-//             }
-//         }
-//     }
-// }
+    public static void permutation(boolean[] board, int tq, int qpsf, String ans) {
+        if (qpsf == tq){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<board.length;i++){
+            if(board[i] == false){
+                board[i] = true; 
+                permutation(board, tq, qpsf+1, ans + " q"+qpsf + "_b"+ i);
+                board[i] = false; //backtrack
+            }
+        }
+    }
+}
--- Lec30_Backtracking4/_02_Word_Search.java
+++ Lec30_Backtracking4/_02_Word_Search.java
@@ -1,51 +1,51 @@
-// // package Lec30_Backtracking4;
+// package Lec30_Backtracking4;
 
-// // https://leetcode.com/problems/word-search/description/
-
-
-// public class _02_Word_Search {
-//     public static void main(String[] args) {
-//         char[][] maze = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
-//         String word = "ABCCED";
-//         for (int i = 0; i < maze.length; i++) {
-//             for (int j = 0; j < maze[0].length; j++) {
-//                 if (word.charAt(0) == maze[i][j]) {
-//                     boolean ans = WordSearch(maze, i, j, word, 0);
-//                     if (ans == true) {
-//                         System.out.println(ans);
-//                         return;
-//                     }
-//                 }
-
-//             }
-//         }
-//         System.out.println(false);
-//     }
-
-//     public static boolean WordSearch(char [][] maze, int cr,int cc, String word, int index){
-//         if(index == word.length()){
-//             return true;
-//         }
-//         if (cr < 0 || cc < 0 || cr >= maze.length || cc >= maze[0].length || word.charAt(index) != maze[cr][cc]) {
-//             return false;
-//         }
-
-//         // blocked
-//         maze[cr][cc] = '*';
-//         // left right top bottom
-//         int r[] = {0,0,-1,1};
-//         int c[] = {-1,1,0,0};
-
-//         for(int i=0; i<c.length;i++){
-//             boolean ans = WordSearch(maze, cr+r[i], cc+c[i], word, index+1);
-//             if(ans){
-//                 return true;
-//             }
-//         }
-//         // undo
-//         maze[cr][cc] = word.charAt(index);
-//         return false;
+// https://leetcode.com/problems/word-search/description/
 
 
-//     }
-// }
+public class _02_Word_Search {
+    public static void main(String[] args) {
+        char[][] maze = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+        String word = "ABCCED";
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (word.charAt(0) == maze[i][j]) {
+                    boolean ans = WordSearch(maze, i, j, word, 0);
+                    if (ans == true) {
+                        System.out.println(ans);
+                        return;
+                    }
+                }
+
+            }
+        }
+        System.out.println(false);
+    }
+
+    public static boolean WordSearch(char [][] maze, int cr,int cc, String word, int index){
+        if(index == word.length()){
+            return true;
+        }
+        if (cr < 0 || cc < 0 || cr >= maze.length || cc >= maze[0].length || word.charAt(index) != maze[cr][cc]) {
+            return false;
+        }
+
+        // blocked
+        maze[cr][cc] = '*';
+        // left right top bottom
+        int r[] = {0,0,-1,1};
+        int c[] = {-1,1,0,0};
+
+        for(int i=0; i<c.length;i++){
+            boolean ans = WordSearch(maze, cr+r[i], cc+c[i], word, index+1);
+            if(ans){
+                return true;
+            }
+        }
+        // undo
+        maze[cr][cc] = word.charAt(index);
+        return false;
+
+
+    }
+}
